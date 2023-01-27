47e482145ba942ee79e0ddbf6b7f9c0789504721
It's me Abel <abel97.ag@gmail.com>
2023-01-27 14:43:56 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace MyTextEditor
+{
+    partial class underleaf
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(underleaf));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.texteditor = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.allletters = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Lettersspace = new System.Windows.Forms.ToolStripStatusLabel();
+            this.wordcount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.rowcount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(775, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.toolStripSeparator,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("newToolStripMenuItem.Image")));
+            this.newToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.newToolStripMenuItem.Text = "&New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripMenuItem.Image")));
+            this.openToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(178, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripMenuItem.Image")));
+            this.saveToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.saveAsToolStripMenuItem.Text = "Save &As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(178, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(178, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // texteditor
+            // 
+            this.texteditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.texteditor.Location = new System.Drawing.Point(0, 30);
+            this.texteditor.Name = "texteditor";
+            this.texteditor.Size = new System.Drawing.Size(775, 460);
+            this.texteditor.TabIndex = 1;
+            this.texteditor.Text = "";
+            this.texteditor.TextChanged += new System.EventHandler(this.texteditor_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.allletters,
+            this.Lettersspace,
+            this.wordcount,
+            this.rowcount});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 464);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(775, 26);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // allletters
+            // 
+            this.allletters.Name = "allletters";
+            this.allletters.Size = new System.Drawing.Size(90, 20);
+            this.allletters.Text = "All Letters: 0";
+            // 
+            // Lettersspace
+            // 
+            this.Lettersspace.Name = "Lettersspace";
+            this.Lettersspace.Size = new System.Drawing.Size(164, 20);
+            this.Lettersspace.Text = "Letters without space: 0";
+            // 
+            // wordcount
+            // 
+            this.wordcount.Name = "wordcount";
+            this.wordcount.Size = new System.Drawing.Size(66, 20);
+            this.wordcount.Text = "Words: 0";
+            // 
+            // rowcount
+            // 
+            this.rowcount.Name = "rowcount";
+            this.rowcount.Size = new System.Drawing.Size(59, 20);
+            this.rowcount.Text = "Rows: 0";
+            // 
+            // underleaf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 490);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.texteditor);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "underleaf";
+            this.Text = "Underleaf beta";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox texteditor;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel allletters;
+        private System.Windows.Forms.ToolStripStatusLabel Lettersspace;
+        private System.Windows.Forms.ToolStripStatusLabel wordcount;
+        private System.Windows.Forms.ToolStripStatusLabel rowcount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MyTextEditor
+{
+
+    public partial class underleaf : Form
+    {
+        string filePath = string.Empty;
+        int isfileSaved; //variable used to check if the file has been saved or not
+        int isopen;
+        int allletterscount; //Keeps count of all characters
+        int letterswithoutspace; 
+        //int words;
+        int rows;
+
+        //initilaizes the program
+        public underleaf()
+        {
+            InitializeComponent();
+            this.Text = "Nameless.txt";
+            texteditor.AllowDrop = true;
+            //texteditor.EnableAutoDragDrop = true; //can be used to move text within the editor
+            texteditor.DragDrop += Texteditor_DragDrop;
+        }
+
+        //drag and drop function
+        private void Texteditor_DragDrop(object sender, DragEventArgs e)
+        {
+            //throw new NotImplementedException();
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if(data != null)
+            {
+                String[] dropfiles = (string[])e.Data.GetData(DataFormats.FileDrop, false); //used to get file path
+                if (dropfiles != null && dropfiles.Length != 0)
+                {
+                    System.IO.StreamReader dropreader = new System.IO.StreamReader(dropfiles[0]);
+                    string dropfileTemp = dropreader.ReadToEnd(); //saves the contents of file in a temporary file
+                    dropreader.Close();
+                    //if Ctrl is pressed
+                    if (ModifierKeys.HasFlag(Keys.Control))
+                    {   
+                        underleaf.ActiveForm.Text = underleaf.ActiveForm.Text + '*';
+                        texteditor.SelectionColor = Color.DarkGreen;
+                        texteditor.AppendText(dropfileTemp);
+                    }else if (ModifierKeys.HasFlag(Keys.Shift)) //If Shift is pressed
+                    {
+                        //underleaf.ActiveForm.Text = underleaf.ActiveForm.Text = System.IO.Path.GetFileNameWithoutExtension(filePath) + '*';
+                        texteditor.SelectedText = dropfileTemp;
+                    }
+                    else
+                    {
+                        
+                        DialogResult result = MessageBox.Show("You have not saved the current file. Save the file first?", "Warning!", MessageBoxButtons.YesNoCancel);
+                        if (result == System.Windows.Forms.DialogResult.Yes)
+                        {
+                            texteditor.Text = dropfileTemp;
+                            savefile();
+                            underleaf.ActiveForm.Text = System.IO.Path.GetFileNameWithoutExtension(filePath);
+                            isfileSaved = 2;
+
+                        }
+                        else
+                        { 
+                        
+                        }
+                    }
+                }
+                underleaf.ActiveForm.Text = System.IO.Path.GetFileNameWithoutExtension(filePath);
+            }
+            underleaf.ActiveForm.Text = System.IO.Path.GetFileNameWithoutExtension(filePath);
+        }
+        //creates a new text file
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            if (isfileSaved == 1)
+            {
+                result = MessageBox.Show("You have not saved the current file. Save the file first?", "Warning!", MessageBoxButtons.YesNoCancel);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    savefile();
+
+                    underleaf.ActiveForm.Text = System.IO.Path.GetFileNameWithoutExtension(filePath);
+                }
+                else if (result == System.Windows.Forms.DialogResult.No)
+                {
+                    texteditor.Clear();
+                    filePath = "Nameless";
+                    underleaf.ActiveForm.Text = System.IO.Path.GetFileNameWithoutExtension(filePath);
+                }
+            }
+            else
+            {
+                texteditor.Clear();
+
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            if (isfileSaved == 1)
+            {
+                    Application.Exit();
+                    isfileSaved = 2;
+                
+            }
+            else
+            {
+                Application.Exit();
+
+            }
+        }
+        //Used to open a file
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            if (isfileSaved == 1)
+            {
+                result = MessageBox.Show("You have not saved the current file. Save the file first?", "Warning!", MessageBoxButtons.YesNoCancel);
+
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    savefile();
+
+                } else if (result == System.Windows.Forms.DialogResult.No)
+                {
+                    OpenFileDialog fileopener = new OpenFileDialog();
+                    fileopener.Filter = "Text Files (.txt)|*.txt";
+                    fileopener.Title = "Select file to open ...";
+
+                    if (fileopener.ShowDialog() == DialogResult.OK)
+                    {
+                        //filePath = fileopener.FileName;
+                        //Read the contents of the file into a stream
+                        var fileStream = fileopener.OpenFile();
+                        System.IO.StreamReader reader = new System.IO.StreamReader(fileStream);
+                        texteditor.Text = reader.ReadToEnd();
+
+                        allletterscount = texteditor.TextLength;
+                        allletters.Text = "All Letters: " + allletterscount;
+
+                        String temstring = texteditor.Text;
+                        letterswithoutspace = temstring.Count(char.IsLetterOrDigit);
+                        Lettersspace.Text = "Letters without space: " + letterswithoutspace;
+
+                        rowcount.Text = "Rows:" + texteditor.Lines.Count();
+
+                        int word = wordCounter(texteditor.Text);
+                        wordcount.Text = "Words: " + word;
+                        reader.Close();
+
+                        isfileSaved = 2;
+                    }
+                    underleaf.ActiveForm.Text = System.IO.Path.GetFileNameWithoutExtension(filePath);
+
+                }
+            }
+            else
+            {
+                OpenFileDialog fileopener = new OpenFileDialog();
+                fileopener.Filter = "Text Files (.txt)|*.txt";
+                fileopener.Title = "Select file to open ...";
+
+                if (fileopener.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = fileopener.FileName;
+                    //Read the contents of the file into a stream
+                    var fileStream = fileopener.OpenFile();
+                    System.IO.StreamReader reader = new System.IO.StreamReader(fileStream);
+                    texteditor.Text = reader.ReadToEnd();
+
+                    allletterscount = texteditor.TextLength;
+                    allletters.Text = "All Letters: " + allletterscount;
+
+                    String temstring = texteditor.Text;
+                    letterswithoutspace = temstring.Count(char.IsLetterOrDigit);
+                    Lettersspace.Text = "Letters without space: " + letterswithoutspace;
+
+                    rowcount.Text = "Rows:" + texteditor.Lines.Count();
+
+                    int word = wordCounter(texteditor.Text);
+                    wordcount.Text = "Words: " + word;
+                    reader.Close();
+                    isfileSaved = 2;
+                }
+                underleaf.ActiveForm.Text = System.IO.Path.GetFileNameWithoutExtension(filePath);
+            }
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            savefile();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog filesaver = new SaveFileDialog();
+            filesaver.Filter = "Text Files (.txt)|*.txt";
+            filesaver.Title = "Save file ...";
+            filesaver.FileName = "Nameless";
+
+
+            if (filesaver.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter saver = new System.IO.StreamWriter(filesaver.FileName);
+                filePath = filesaver.FileName;
+                underleaf.ActiveForm.Text = System.IO.Path.GetFileNameWithoutExtension(filePath);
+                saver.Write(texteditor.Text);
+                saver.Close();
+            }
+
+            isfileSaved = 2;
+        }
+
+        //when the contents of the text field has been changed
+        private void texteditor_TextChanged(object sender, EventArgs e)
+        {
+            if (filePath == String.Empty)
+            {
+                underleaf.ActiveForm.Text = "Nameless" + "*";
+                isfileSaved = 1;
+                allletterscount = texteditor.TextLength;
+                allletters.Text = "All Letters: " + allletterscount;
+
+                String temstring = texteditor.Text;
+                letterswithoutspace = temstring.Count(char.IsLetterOrDigit);
+                Lettersspace.Text = "Letters without space: " + letterswithoutspace;
+
+                rowcount.Text = "Rows:" + texteditor.Lines.Count();
+
+                int words = wordCounter(texteditor.Text);
+                wordcount.Text = "Words: " + words;
+            }
+            else
+            {
+                underleaf.ActiveForm.Text = System.IO.Path.GetFileNameWithoutExtension(filePath) + "*";
+                isfileSaved = 1;
+            }
+
+        }
+
+        //Saves as file
+        private void savefile()
+        {
+            if (filePath == String.Empty)
+            {
+                SaveFileDialog filesaver = new SaveFileDialog();
+                filesaver.Filter = "Text Files (.txt)|*.txt";
+                filesaver.Title = "Save file ...";
+                filesaver.FileName = "Nameless";
+
+                if (filesaver.ShowDialog() == DialogResult.OK)
+                {
+                    System.IO.StreamWriter saver0 = new System.IO.StreamWriter(filesaver.FileName);
+                    saver0.Write(texteditor.Text);
+                    saver0.Close();
+                }
+                filePath = filesaver.FileName;
+                isfileSaved = 2;
+            }
+            else
+            {
+                isfileSaved = 2;
+                System.IO.StreamWriter saver = new System.IO.StreamWriter(filePath);
+                saver.Write(texteditor.Text);
+                saver.Close();
+            }
+        }
+        //counts characters excluding space
+        private static int wordCounter(string s)
+        {
+            return s.Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        //triggers upon exiting the program
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            DialogResult result;
+            if (isfileSaved == 1)
+            {
+                result = MessageBox.Show("You have not saved the current file. Save the file first?", "Warning!", MessageBoxButtons.YesNoCancel);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    savefile();
+                    isfileSaved = 2;
+                }
+                else if (result == System.Windows.Forms.DialogResult.No)
+                {
+                    
+                    isfileSaved = 2;
+                }
+                else if (result == System.Windows.Forms.DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                    isfileSaved = 1;
+                }
+            }
+            else
+            {
+                Application.Exit();
+
+            }
+        }
+    }
+}
